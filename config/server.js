27296983f4da0e97f720c7ca07ff11c7a22fb916
lulaluano <luanferreira2136@gmail.com>
2021-05-20 01:46:09 -0300

    another build

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
+  url: 'https://brcadv.com/strapi',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2e48878df91bba73d7f2365772c4dade'),
