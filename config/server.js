bd443f8e3f5754ac08d0864c619ed8203521f3a6
lulaluano <luanferreira2136@gmail.com>
2021-05-20 01:01:25 -0300

    changed port to 1338

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2e48878df91bba73d7f2365772c4dade'),
