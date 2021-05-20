73c0e2d687a9b851545aa098ffd5beb350f554f5
lulaluano <luanferreira2136@gmail.com>
2021-05-20 00:36:36 -0300

    include build files

---
+++ .cache/admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const LeftMenuListLink = styled.div`
+  max-height: 500px;
+  margin-bottom: 19px;
+  margin-right: 25px;
+  overflow: auto;
+
+  /*&:not(&:nth-child(2)) {
+    display: none;
+  }*/
+`;
+
+export default LeftMenuListLink;
